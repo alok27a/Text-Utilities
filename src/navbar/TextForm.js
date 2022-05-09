d8667203d5ecdf989d08503849f822a78be230f0
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2022-05-09 22:56:07 +0530

    Added Other Features

--- src/navbar/TextForm.js
+++ src/navbar/TextForm.js
@@ -18,6 +18,11 @@ export default function (props) {
         setText(newText);
     }
 
+    const lowerCaseClicked = (event) => {
+        setText(text.toLowerCase());
+    }
+
+
   return (
     <>
      <h3 >{props.heading}</h3>
@@ -32,6 +37,15 @@ export default function (props) {
         />
       </div>
         <button className="btn btn-primary" onClick={upperCaseClicked}>Convert To Uppercase </button>
+        <button className="btn btn-primary mx-3" onClick={lowerCaseClicked}>Convert To Uppercase </button>
+        <div className="container my-3">
+            <h4>Given Number of Words and Character</h4>
+            <p>{text.split(" ").length } Words {text.length} Characters</p>
+        </div>
+        <div className="container my-3"> 
+            <h4>Preview</h4>
+            <p>{text}</p>
+        </div> 
     </> 
   );
 }
